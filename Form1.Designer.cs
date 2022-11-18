c2bf5e6f4705fc0527d018f07ec60253c80528a3
Gouvernon Stan <GouvernonSt@s2.rpn.ch>
2022-11-18 12:25:33 +0100

    Amélioration de l'affichage

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,14 +34,27 @@ namespace Flexion
             this.tbxFlexion = new System.Windows.Forms.TextBox();
             this.nudForce = new System.Windows.Forms.NumericUpDown();
             this.nudDeformation = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblLongueur = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.lblHauteur = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudForce)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudDeformation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // lblForce
             // 
             this.lblForce.AutoSize = true;
-            this.lblForce.Location = new System.Drawing.Point(13, 32);
+            this.lblForce.Location = new System.Drawing.Point(12, 42);
             this.lblForce.Name = "lblForce";
             this.lblForce.Size = new System.Drawing.Size(51, 13);
             this.lblForce.TabIndex = 0;
@@ -58,7 +71,7 @@ namespace Flexion
             // 
             // tbxFlexion
             // 
-            this.tbxFlexion.Location = new System.Drawing.Point(51, 133);
+            this.tbxFlexion.Location = new System.Drawing.Point(49, 362);
             this.tbxFlexion.Name = "tbxFlexion";
             this.tbxFlexion.ReadOnly = true;
             this.tbxFlexion.Size = new System.Drawing.Size(155, 20);
@@ -67,7 +80,7 @@ namespace Flexion
             // nudForce
             // 
             this.nudForce.DecimalPlaces = 1;
-            this.nudForce.Location = new System.Drawing.Point(106, 30);
+            this.nudForce.Location = new System.Drawing.Point(125, 40);
             this.nudForce.Name = "nudForce";
             this.nudForce.Size = new System.Drawing.Size(120, 20);
             this.nudForce.TabIndex = 5;
@@ -76,17 +89,101 @@ namespace Flexion
             // nudDeformation
             // 
             this.nudDeformation.DecimalPlaces = 1;
-            this.nudDeformation.Location = new System.Drawing.Point(106, 70);
+            this.nudDeformation.Location = new System.Drawing.Point(126, 70);
             this.nudDeformation.Name = "nudDeformation";
             this.nudDeformation.Size = new System.Drawing.Size(120, 20);
             this.nudDeformation.TabIndex = 6;
             this.nudDeformation.ValueChanged += new System.EventHandler(this.nudDeformation_ValueChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Location = new System.Drawing.Point(126, 103);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // lblLongueur
+            // 
+            this.lblLongueur.AutoSize = true;
+            this.lblLongueur.Location = new System.Drawing.Point(13, 105);
+            this.lblLongueur.Name = "lblLongueur";
+            this.lblLongueur.Size = new System.Drawing.Size(75, 13);
+            this.lblLongueur.TabIndex = 7;
+            this.lblLongueur.Text = "Longueur (cm)";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 1;
+            this.numericUpDown2.Location = new System.Drawing.Point(125, 133);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 10;
+            // 
+            // lblHauteur
+            // 
+            this.lblHauteur.AutoSize = true;
+            this.lblHauteur.Location = new System.Drawing.Point(12, 135);
+            this.lblHauteur.Name = "lblHauteur";
+            this.lblHauteur.Size = new System.Drawing.Size(68, 13);
+            this.lblHauteur.TabIndex = 9;
+            this.lblHauteur.Text = "Hauteur (cm)";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 1;
+            this.numericUpDown3.Location = new System.Drawing.Point(125, 163);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Largeur (cm)";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(125, 193);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nombre de matériaux";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(49, 237);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.lblHauteur);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblLongueur);
             this.Controls.Add(this.nudDeformation);
             this.Controls.Add(this.nudForce);
             this.Controls.Add(this.tbxFlexion);
@@ -97,6 +194,10 @@ namespace Flexion
             this.Text = "Flexion TIP";
             ((System.ComponentModel.ISupportInitialize)(this.nudForce)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudDeformation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,6 +210,15 @@ namespace Flexion
         private System.Windows.Forms.TextBox tbxFlexion;
         private System.Windows.Forms.NumericUpDown nudForce;
         private System.Windows.Forms.NumericUpDown nudDeformation;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblLongueur;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label lblHauteur;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
