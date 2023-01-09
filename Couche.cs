43cb09376a80ba15f2fbc70dcb4d8f1686c61ab0
GouvernonSt <stan.gouvernon@rpn.ch>
2023-01-09 14:53:13 +0000

    Update Couche.cs

--- Couche.cs
+++ Couche.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -52,6 +52,50 @@ namespace Flexion
         {
             double Base = Longueur * LargeurCenter;
             return (Base * Math.Pow(HauteurCenter, 3)) / 12 + HauteurCenter * Base * Z*Z;
+        }
+
+        public double double Surface()
+        {
+            double Lf, Li = Largeur();
+            double Ef, Ei = Hauteur();
+            double Sf = Lf * Ef;
+            double Si = Li * Ei;
+            return Sf,Si;
+        }
+
+        public double double Largeur()
+        {
+            double L1 = (4 * Le1 - 4 * Lp1) / (Lo * Lo);
+            double L2 = (x - Lo / 2) * (x - Lo / 2);
+            double L3 = (Xi - Lo / 2) * (Xi - Lo / 2);
+            double Lf = L11 * L12 + Lp1;
+            double Li = L11 * L13 + Lp1;
+            return Lf,Li
+        }
+
+        public double double Hauteur()
+        {
+            double E1 = (4 * Ee1 - 4 * Ep1) / (Lo * *2);
+            double E2 = (x - Lo / 2) * *2;
+            double E3 = (Xi - Lo / 2) * *2;
+            double Ef = E11 * E12 + Ep1;
+            double Ei = E11 * E13 + Ep1;
+            return Ef,Ei;
+        }
+
+        public double Integrale(double e, double p,bool temp)
+        {
+            double X113 = (((4 * e) - (4 * p)) / (3 * (Lo * *2))) * Math.Pow(Xi,3)
+            double X112 = (((2 * e) - (2 * p)) / Lo) * (Xi * *2)
+            double X111 = e * Xi
+            double X103 = (((4 * e) - (4 * p)) / (3 * (Lo * *2))) * Math.Pow(Dep, 3)
+            double X102 = (((2 * e) - (2 * p)) / Lo) * (Dep * *2)
+            double X101 = e * Dep
+
+            double Int11 = (X113 - X112 + X111) - (X103 - X102 - X101);
+            double B1 = Int11 / Xi;
+            if(temp) { return B1; }
+            else { return B1f = B1 / Eref * Ela1; }
         }
     }
 }
