31993ff5b37ccd2da573e55e7e4f402a0b898075
Gouvernon <stan@gouvernon.org>
2023-09-07 16:10:23 +0200

    Début de la sauvegarde dans Settings.settings.

---
+++ Sauvegarde.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flexion
+{
+    public static class Sauvegarde
+    {
+        public static string GetMatières()
+        {
+            try { return }
+        }
+    }
+}
