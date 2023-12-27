f4c55c051b82f582e441f4a35d9b7018e8faedf9
Gouvernon Stan <stan@gouvernon.org>
2023-12-27 10:00:57 +0100

    began the creation of the material editor

---
+++ FlexionV2/Views/MaterialEditor.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FlexionV2.Views;
+
+public partial class MaterialEditor : Window
+{
+    public MaterialEditor()
+    {
+        InitializeComponent();
+    }
+}
