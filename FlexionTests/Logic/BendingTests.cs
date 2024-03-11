73120dda7e494d3a07a53e0f03d4a97c766c1cc6
Gouvernon <stan@gouvernon.org>
2024-03-11 14:58:59 +0100

    Improved the quatity of test for the bending

--- FlexionTests/Logic/BendingTests.cs
+++ FlexionTests/Logic/BendingTests.cs
@@ -6,7 +6,32 @@ namespace FlexionTests.Logic;
 public class BendingTests
 {
     [Fact]
-    public void CreateNewLayerTest()
+    public void SingleLayerRectangularPieceBendingTest()
+    {
+        Piece piece = new(0.77,"Test")
+        {
+            Layers = new List<Layer>
+            {
+                new()
+                {
+                    WidthAtCenter = 0.045,
+                    WidthOnSides = 0.045,
+                    HeightAtCenter = 0.01,
+                    HeightOnSides = 0.01,
+                    Material = new Material
+                    {
+                        Name = "Bois",
+                        E = 17000000000
+                    }
+                }
+            }
+        };
+        IEnumerable<double> values = piece.CalculateFlexion(100, piece.Length / 10000);
+        Assert.Equal(-14.924,values.Min()*1000,0.001);
+    }
+    
+    [Fact]
+    public void MultiLayerRectangularPieceBendingTest()
     {
         Piece piece = new(0.77,"Test")
         {
@@ -52,5 +77,54 @@ public class BendingTests
         };
         IEnumerable<double> values = piece.CalculateFlexion(100, piece.Length / 10000);
         Assert.Equal(-2.609,values.Min()*1000,0.001);
+    }
+    
+    [Fact]
+    public void MultiLayerPieceBendingTest()
+    {
+        Piece piece = new(0.77,"Test")
+        {
+            Layers = new List<Layer>
+            {
+                new()
+                {
+                    WidthAtCenter = 0.03,
+                    WidthOnSides = 0.04,
+                    HeightAtCenter = 0.002,
+                    HeightOnSides = 0.004,
+                    Material = new Material
+                    {
+                        Name = "Alu",
+                        E = 69000000000
+                    }
+                },
+                new()
+                {
+                    WidthAtCenter = 0.05,
+                    WidthOnSides = 0.045,
+                    HeightAtCenter = 0.012,
+                    HeightOnSides = 0.01,
+                    Material = new Material
+                    {
+                        Name = "Bois",
+                        E = 17000000000
+                    }
+                },
+                new()
+                {
+                    WidthAtCenter = 0.03,
+                    WidthOnSides = 0.04,
+                    HeightAtCenter = 0.002,
+                    HeightOnSides = 0.004,
+                    Material = new Material
+                    {
+                        Name = "Alu",
+                        E = 69000000000
+                    }
+                }
+            }
+        };
+        IEnumerable<double> values = piece.CalculateFlexion(100, piece.Length / 10000);
+        Assert.Equal(-1.653,values.Min()*1000,0.001);
     }
 }
